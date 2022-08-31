ed1ff557ed169eec42b4f544814dcfe00d7cad0c
Islam Shehata <islam@axiom.co>
2022-08-31 14:35:45 +0200

    loop over logs&traces flush handlers

--- examples/prisma-axiom/index.ts
+++ examples/prisma-axiom/index.ts
@@ -1,9 +1,12 @@
 import withAxiom from '../../src/axiom';
 import { PrismaClient } from '@prisma/client';
+const express = require('express')
+const app = express()
 
-async function main() {
-  const prisma = withAxiom(new PrismaClient());
+const client = new PrismaClient();
+const prisma = withAxiom(client);
 
+app.get('/', async (_: any, res: any) => {
   await prisma.user.create({
     data: {
       name: 'Alice',
@@ -15,6 +18,19 @@ async function main() {
   console.log('new user created', user);
 
   await prisma.user.deleteMany();
-}
 
-main();
+  res.send('hello world')
+});
+
+const server = app.listen(3000)
+
+
+process.on('SIGINT', () => {
+  console.log('SIGINT signal received: closing HTTP server')
+  // call prisma disconnect t ensure logs and traces are flushed
+  prisma.$disconnect().finally(() => {
+    console.log('disconnect');
+    // server.close();
+    process.exit(0)
+  });
+})
