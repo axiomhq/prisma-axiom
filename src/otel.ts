3d2c03ef83613a85137be010d7c4ce5ce9a029cf
Islam Shehata <islam@axiom.co>
2022-09-14 18:04:07 +0200

    set axiom provider as global provider if none is set

--- src/otel.ts
+++ src/otel.ts
@@ -5,6 +5,7 @@ import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { PrismaInstrumentation } from '@prisma/instrumentation';
 import { Resource } from '@opentelemetry/resources';
+import { trace } from '@opentelemetry/api'
 
 const Version = require('../package.json').version;
 
@@ -15,6 +16,7 @@ export function otelTracerProvider(
 ): NodeTracerProvider {
   const exporter = otelTraceExporter(axiomUrl, axiomToken);
 
+
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: process.env.npm_package_name,
@@ -22,6 +24,10 @@ export function otelTracerProvider(
   });
 
   provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+
+  if (trace.setGlobalTracerProvider(provider) == false) {
+    provider.register()
+  }
 
   registerInstrumentations({
     tracerProvider: provider,
